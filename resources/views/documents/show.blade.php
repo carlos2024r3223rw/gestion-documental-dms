51b95cff6e686a700f196ddd4b7b0d9e6175650e
carlos2024r3223rw <carlos.martinez24@itca.edu.sv>
2026-08-03 15:45:26 -0600

    style: make background darker (slate-100) for better card contrast

--- resources/views/documents/show.blade.php
+++ resources/views/documents/show.blade.php
@@ -17,7 +17,7 @@
         </div>
     </x-slot>
 
-    <div class="py-12 bg-slate-50 min-h-screen">
+    <div class="py-12 bg-slate-100 min-h-screen">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-8">
             
             @if(session('success'))
